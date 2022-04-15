07b79ca8a7d07516dfce4df8b1889b5b5615b96f
Vincenzo117 <94491363+Vincenzo117@users.noreply.github.com>
2022-04-15 13:58:06 +0200

    small fix

--- src/App.vue
+++ src/App.vue
@@ -22,7 +22,7 @@ export default {
 <style lang="scss">
 // @apply positioning dimension-properties(grow/shrink/...) height/width margin/padding/border backgorund text kids-layout(flex/grid/...) other
 #app {
-  @apply min-h-screen w-screen pt-20 bg-[#141414] text-white flex flex-col;
+  @apply min-h-screen w-screen pt-36 md:pt-20 bg-[#141414] text-white flex flex-col;
   font-family: Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
@@ -36,6 +36,7 @@ export default {
 .slide-leave-active {
   transition: opacity 1s, scale 1s, transform 1s;
 }
+
 .slide-enter,
 .slide-leave {
   opacity: 0;
